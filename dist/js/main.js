25c1cb3efe47898587887221e5c35e14cc09917c
matcook <mat.cook@bigpond.com>
2018-10-13 15:05:07 +0800

    javascript added

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,26 +1,74 @@
-function makeChart(name, amount) {
+import jump from '../../node_modules/jump.js/dist/jump.module.js';
+
+// Sticky Nav
+
+window.onscroll = () => {
+  stickyNav();
+  console.log(navbar.offsetTop);
+}
+
+let about = document.querySelector('#about');
+let navbar = document.querySelector('.navbar');
+
+let stickyNav = () => {
+  if (window.pageYOffset >= about.offsetTop - 60) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+
+}
+
+
+// Smooth Scrolling Buttons
+
+let homeBtn = document.querySelectorAll('li')[0];
+let aboutBtn = document.querySelectorAll('li')[1];
+let projectsBtn = document.querySelectorAll('li')[2];
+let contactBtn = document.querySelectorAll('li')[3];
+let enterBtn = document.querySelector('.btn');
+
+enterBtn.addEventListener('click', () => {
+  jump('#about');
+});
+
+homeBtn.addEventListener('click', () => {
+  jump('#home');
+});
+
+aboutBtn.addEventListener('click', () => {
+  jump('#about');
+});
+
+projectsBtn.addEventListener('click', () => {
+  jump('#projects');
+});
+
+contactBtn.addEventListener('click', () => {
+  jump('#contact');
+});
+
+// Skill Charts
+
+let makeChart = (name, amount) => {
   let options = {
     chart: {
-      width: '100%',
+      width: "100%",
 
-      type: 'radialBar',
+      type: "radialBar"
     },
     plotOptions: {
       radialBar: {
         hollow: {
-          size: '65%',
+          size: "65%"
         }
-      },
+      }
     },
     series: [amount],
-    labels: [name.id],
+    labels: [name.id]
+  };
 
-  }
-
-  let chart = new ApexCharts(
-    name,
-    options
-  );
+  let chart = new ApexCharts(name, options);
   chart.render();
 }
 
