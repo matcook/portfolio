c88a3d343d94e91f5f8bb9fa4a1ce7d420d05083
matcook <mat.cook@bigpond.com>
2018-10-13 12:17:10 +0800

    about layout with apexcharts

---
+++ dist/js/main.js
@@ -0,0 +1,36 @@
+function makeChart(name, amount) {
+  let options = {
+    chart: {
+      width: '100%',
+
+      type: 'radialBar',
+    },
+    plotOptions: {
+      radialBar: {
+        hollow: {
+          size: '70%',
+        }
+      },
+    },
+    series: [amount],
+    labels: [name.id],
+
+  }
+
+  console.log(name);
+  let chart = new ApexCharts(
+    name,
+    options
+  );
+  chart.render();
+}
+
+makeChart(HTML, 80);
+makeChart(CSS3, 90);
+makeChart(JavaScript, 90);
+makeChart(React, 90);
+makeChart(NodeJS, 90);
+makeChart(PHP, 90);
+makeChart(SQL, 90);
+
+makeChart(Google, 90);
