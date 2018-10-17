1af3f00007f1378dabbae7f38a0b61d239ef93f4
matcook <mat.cook@bigpond.com>
2018-10-17 11:53:28 +0800

    graph tag font-size lowered

--- dist/js/main.js
+++ dist/js/main.js
@@ -9,7 +9,7 @@ let runGraph = () => {
 
   for (let i = 0; i < bar.length; i++) {
     let percent = bar[i].dataset.percent;
-    bar[i].style.width = `calc(${percent} - 100px)`;
+    bar[i].style.width = `calc(${percent} - 80px)`;
   }
 };
 2;
