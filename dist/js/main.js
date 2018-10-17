32da0a0dabd48836e05362992b9b4f916a46fa8f
matcook <mat.cook@bigpond.com>
2018-10-17 13:22:33 +0800

    sticky navbar

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,8 +1,23 @@
 window.onscroll = () => {
-  if (pageYOffset >= 100) {
+  if (pageYOffset >= document.querySelector('#about').offsetTop) {
     runGraph();
   }
+
+  if (pageYOffset >= document.querySelector('#about').offsetTop - 60) {
+    console.log('sticky');
+    document.querySelector('.navbar').classList.add('sticky');
+    document.querySelector('#about').style.marginTop = '60px';
+  } else {
+    document.querySelector('.navbar').classList.remove('sticky');
+    document.querySelector('#about').removeAttribute('style');
+  }
+
 };
+
+
+
+
+
 
 let runGraph = () => {
   let bar = document.querySelectorAll(".bar-fill");
@@ -12,4 +27,3 @@ let runGraph = () => {
     bar[i].style.width = `calc(${percent} - 80px)`;
   }
 };
-2;
