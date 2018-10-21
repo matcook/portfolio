1bcff3b5459e85a69cc730efb42d56e92162f419
matcook <mat.cook@bigpond.com>
2018-10-21 21:12:19 +0800

    active links

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,43 +1,63 @@
+let navbar = document.querySelector(".navbar");
+let navBtn = document.querySelectorAll("li");
+let skills = document.querySelector(".skills");
+let about = document.querySelector("#about");
+let projects = document.querySelector("#projects");
+let contact = document.querySelector("#contact");
+
 window.onload = () => {
   navBtns();
 
-  if (pageYOffset > document.querySelector('.skills') - 200) {
+  if (pageYOffset > skills.offsetTop - 300) {
     runGraph();
   }
-}
-
-
-window.onscroll = () => {
-  if (pageYOffset >= document.querySelector('.skills').offsetTop - 300) {
-    runGraph();
-  }
-
-  if (pageYOffset >= document.querySelector('#about').offsetTop - 60) {
-    console.log('sticky');
-    document.querySelector('.navbar').classList.add('sticky');
-    document.querySelector('#about').style.marginTop = '60px';
-  } else {
-    document.querySelector('.navbar').classList.remove('sticky');
-    document.querySelector('#about').removeAttribute('style');
-  }
-
-
-
 };
 
-document.querySelector('.btn').addEventListener('click', () => {
-  scrollTo(document.documentElement, document.querySelector('#about').offsetTop - 60, 200);
-})
+window.onscroll = () => {
+  if (pageYOffset >= skills.offsetTop - 300) {
+    runGraph();
+  }
+
+  if (pageYOffset >= about.offsetTop - 60) {
+    navbar.classList.add("sticky");
+    about.style.marginTop = "60px";
+  } else {
+    navbar.classList.remove("sticky");
+    about.removeAttribute("style");
+  }
+};
+
+document.querySelector(".btn").addEventListener("click", () => {
+  scrollTo(document.documentElement, about.offsetTop - 60, 200);
+});
 
 let navBtns = () => {
-  let navBtn = document.querySelectorAll('li');
-
   for (let i = 0; i < navBtn.length; i++) {
-    navBtn[i].addEventListener('click', () => {
-      scrollTo(document.documentElement, document.querySelector(navBtn[i].dataset.destination).offsetTop - 60, 200);
-    })
+    navBtn[i].addEventListener("click", (e) => {
+      scrollTo(
+        document.documentElement,
+        document.querySelector(navBtn[i].dataset.destination).offsetTop - 60,
+        200
+      );
+      for (let i = 0; i < navBtn.length; i++) {
+        if (navBtn[i] != e.target) {
+          navBtn[i].classList.remove('active');
+        } else {
+          navBtn[i].classList.add('active');
+        }
+      }
+    });
   }
-}
+};
+
+let runGraph = () => {
+  let bar = document.querySelectorAll(".bar-fill");
+
+  for (let i = 0; i < bar.length; i++) {
+    let percent = bar[i].dataset.percent;
+    bar[i].style.width = `calc(${percent} - 80px)`;
+  }
+};
 
 let scrollTo = (element, to, duration) => {
   let start = element.scrollTop,
@@ -54,20 +74,11 @@ let scrollTo = (element, to, duration) => {
     }
   };
   animateScroll();
-}
+};
 
 Math.easeInOutQuad = function (t, b, c, d) {
   t /= d / 2;
-  if (t < 1) return c / 2 * t * t + b;
+  if (t < 1) return (c / 2) * t * t + b;
   t--;
-  return -c / 2 * (t * (t - 2) - 1) + b;
-};
-
-let runGraph = () => {
-  let bar = document.querySelectorAll(".bar-fill");
-
-  for (let i = 0; i < bar.length; i++) {
-    let percent = bar[i].dataset.percent;
-    bar[i].style.width = `calc(${percent} - 80px)`;
-  }
+  return (-c / 2) * (t * (t - 2) - 1) + b;
 };
